5e5f2bf21dc9387e9962b39544ead905b0cf3543
sreenidhimadireddy <sreenidhimadireddy31@gmail.com>
2025-11-02 16:39:23 -0500

    Initial commit

---
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.marsphotos.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SkyBlue80 = Color(0xFFB3E5FC)
+val SkyGrey80 = Color(0xFFE0F7FA)
+val SkyWhite80 = Color(0xFFFFFFFF)
+val SkyBlue40 = Color(0xFF0288D1)
+val SkyGrey40 = Color(0xFF01579B)
+val SkyTeal40 = Color(0xFF4DD0E1)
